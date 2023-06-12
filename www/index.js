a63a746d25177533de19455d7fb4779199e0556b
Aditya Subramanian <aditarun2008@gmail.com>
2023-06-12 02:04:15 -0400

    bootstrap changes

--- www/index.js
+++ www/index.js
@@ -1,4 +1,2 @@
-import("./node_modules/wasm-game-of-life/wasm_game_of_life.js").then((js) => {
-  js.greet("WebAssembly with npm");
-});
-
+import * as wasm from "wasm-game-of-life";
+wasm.greet("Aditya");
